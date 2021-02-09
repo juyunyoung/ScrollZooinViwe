0371bf2b56e77e08f35c017dc8b256cbc7c33696
juyunyouong <yunyoung.ju@gmail.com>
2021-02-09 15:07:57 +0900

    add

--- ScrollZooinViwe/ViewController.swift
+++ ScrollZooinViwe/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIScrollViewDelegate{
+    var image=["bg1.png","bg2.png","bg3.png","bg4.png"]
+    var frame = CGRect.zero
 
+    let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPage = 0
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+       // pageControl.isUserInteractionEnabled = false
+
+        return pageControl
+    }()
+    
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView(frame: self.view.frame)
+        scrollView.showsHorizontalScrollIndicator = false;
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.backgroundColor = UIColor.blue
+        return scrollView
+        
+    }()
+
+    
+    
+ /*   func inint(_ imageName:[String]){
+        for i in 0 ... imageName.count{
+            image.append(UIImage(named: imageName[i])!)
+        }
+        
+    }
+   */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.addSubview(scrollView)
+        pageControl.numberOfPages = image.count
+        setupScreens()
+        self.view.addSubview(self.pageControl)
+
+    }
+    func setupScreens() {
+        for index in 0..<image.count {
+            frame.origin.x = scrollView.frame.size.width * CGFloat(index)
+            frame.size = scrollView.frame.size
+            let imageScrollView = ImageScrollView(frame: frame)
+            imageScrollView.set(image: UIImage(named: image[index])!)
+            //imageScrollView.delegate = self
+            self.scrollView.addSubview(imageScrollView)
+        }
+
+        scrollView.contentSize = CGSize(width: (scrollView.frame.size.width * CGFloat(image.count)), height: scrollView.frame.size.height)
+        scrollView.delegate = self
     }
 
-
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControl.currentPage = Int(pageNumber)
+     
+    }
 }
 
